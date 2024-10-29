6909b7d6585980af3795c4aba327b7639f2c1bb8
enkooo <pytlos1337@gmail.com>
2024-10-29 22:58:18 +0100

    feat: adjust about section on xl displays

--- components/AboutImages.vue
+++ components/AboutImages.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="mx-auto grid size-80 grid-cols-7 grid-rows-7 gap-4">
+  <div class="mx-auto grid size-80 grid-cols-7 grid-rows-7 gap-4 xl:size-[37rem]">
     <div class="col-start-1 col-end-4 row-start-1 row-end-4 rounded-md bg-gray-300 shadow-md"></div>
     <div class="col-start-2 col-end-4 row-start-4 row-end-6 rounded-md bg-gray-300 shadow-md"></div>
     <div class="col-start-4 col-end-8 row-start-2 row-end-8 rounded-md bg-gray-300 shadow-md"></div>
--- components/AboutSection.vue
+++ components/AboutSection.vue
@@ -66,7 +66,7 @@ const BENEFIT_SECTION_ITEMS = computed(() => [
         <span class="hidden md:inline"> - </span>
         {{ $t('about.companyWhoAreWe') }}
       </h3>
-      <div class="text-typography-light flex flex-col gap-6 md:col-start-2 md:row-span-2">
+      <div class="flex flex-col gap-6 text-typography-light md:col-start-2 md:row-span-2">
         <p>
           <span class="text-black"> {{ $t('about.companyName') }} </span>&nbsp;
           {{ $t('about.shortText') }}
@@ -74,9 +74,9 @@ const BENEFIT_SECTION_ITEMS = computed(() => [
         <p>{{ $t('about.description') }}</p>
         <p>{{ $t('about.communication') }}</p>
       </div>
-      <AboutImages />
+      <AboutImages class="xl:row-span-2" />
       <div
-        class="flex flex-col gap-8 md:col-start-1 md:col-end-3 md:row-start-4 md:grid md:grid-cols-2 md:grid-rows-2"
+        class="flex flex-col gap-8 md:col-start-1 md:col-end-3 md:row-start-4 md:grid md:grid-cols-2 md:grid-rows-2 xl:col-start-2 xl:row-start-4"
       >
         <AboutFeatureCard
           v-for="feature in FEATURE_SECTION_ITEMS"
@@ -89,7 +89,7 @@ const BENEFIT_SECTION_ITEMS = computed(() => [
     </div>
     <div class="container mt-28 text-center">
       <h3 class="text-3xl font-bold text-black">{{ $t('whyChooseUs.heading') }}</h3>
-      <p class="text-typography-light mt-7 px-16">{{ $t('whyChooseUs.description') }}</p>
+      <p class="mt-7 px-16 text-typography-light">{{ $t('whyChooseUs.description') }}</p>
       <div
         class="mt-14 flex flex-col place-items-center items-center justify-center gap-10 px-5 sm:grid sm:grid-cols-2 sm:gap-10 md:px-32 lg:grid-cols-4 lg:px-0"
       >
